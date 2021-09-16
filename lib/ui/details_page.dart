7a189b929a9514669adfb2125818ab3655911b81
mehedi hasan <mhasan172036@mscse.uiu.ac.bd>
2021-09-16 08:46:23 +0600

    2nd commit

--- lib/ui/details_page.dart
+++ lib/ui/details_page.dart
@@ -10,23 +10,143 @@ class DetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
+        scrollDirection: Axis.vertical,
         children: [
-          Hero(
-            tag: productModel!.image!,
-            child: Image.network("jdkj"),
-          ),
           Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(10.0),
-            child: Text(
-              productModel!.id.toString(),
-              style: TextStyle(fontSize: 20.0),
+            width: 374.88,
+            height: 240.74,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(0),
+                topRight: Radius.circular(0),
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+              ),
+            ),
+            // child: Image.network(""),
+          ),
+          Text(
+            "Lorem ipsum dolor sit amet.",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
             ),
           ),
+          SizedBox(height: 10),
           Container(
-            alignment: Alignment.topRight,
-            margin: EdgeInsets.all(5.0),
-            child: Text(productModel!.bookingPrice.toString()),
+            width: 375,
+            height: 160.91,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(0),
+                topRight: Radius.circular(0),
+                bottomLeft: Radius.circular(15),
+                bottomRight: Radius.circular(15),
+              ),
+              color: Colors.white,
+            ),
+            child: ListTile(
+              leading: Padding(
+                padding: const EdgeInsets.only(top: 2),
+                child: Container(
+                  width: MediaQuery.of(context).size.width / 2.0,
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Text(
+                              " BDT 1,225",
+                              style: TextStyle(
+                                color: Color(0xffdd3935),
+                                fontSize: 15,
+                                fontFamily: "Roboto",
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Text(
+                              " BDT 2,000",
+                              style: TextStyle(
+                                color: Color(0xff616161),
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                        ],
+                      ),
+                      Expanded(
+                        child: Text(
+                          "Select Color",
+                          style: TextStyle(
+                            color: Color(0xff242424),
+                            fontSize: 15,
+                            fontFamily: "Roboto",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text("data"),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              trailing: Container(
+                width: 63.33,
+                height: 23.66,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Color(0xffdd3935),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Text(
+                    "50% Off",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontFamily: "Roboto",
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ),
         ],
       ),
